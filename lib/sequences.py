ec09a5b31f10b8cf57dd31de3ccb97a1822de4e8
Stephen <skaranja654@gmail.com>
2023-05-18 09:32:39 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+# my_list = [0, 1, 1, 2, 3, 5, 8, 13, 21]
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+
+    if length == 0:
+        print(my_list)
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    elif length == 10:
+        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+    
